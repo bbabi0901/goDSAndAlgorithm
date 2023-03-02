7bae32bf21d3b43c70f14dc85601690a06d60531
bbabi0901 <69451349+bbabi0901@users.noreply.github.com>
2023-03-02 17:46:12 +0900

    DFS, BFS

--- dataStructure/tree.go
+++ dataStructure/tree.go
@@ -5,23 +5,52 @@ type TreeNode struct {
 	Children []*TreeNode
 }
 
-type Tree struct {
-	Root *TreeNode
-}
-
-func (t *Tree) AddNode(data interface{}) {
-	if t.Root == nil {
-		t.Root = &TreeNode{Data: data}
+func (tn *TreeNode) AddNode(data interface{}) bool {
+	// Root 없으면 false, 있으면 자식 추가하고 true
+	if tn.Data == nil {
+		return false
 	} else {
-		t.Root.Children = append(t.Root.Children, &TreeNode{Data: data})
+		tn.Children = append(tn.Children, &TreeNode{Data: data})
+		return true
 	}
 }
 
-func InitTree() *Tree {
-	var tree *Tree = &Tree{Root: InitTreeNode(nil)}
-	return tree
+func MakeTreeNode(data interface{}) *TreeNode {
+	return &TreeNode{Data: data}
 }
 
-func InitTreeNode(data interface{}) *TreeNode {
-	return &TreeNode{Data: data}
+// preorder
+func DFSWithRecursive(startNode *TreeNode) []interface{} {
+	var result []interface{}
+	result = append(result, startNode.Data)
+	for _, child := range startNode.Children {
+		result = append(result, DFSWithRecursive(child)...)
+	}
+	return result
+}
+
+func DFSWithStack(startNode *TreeNode) []interface{} {
+	var stack []*TreeNode
+	var result []interface{}
+	stack = append(stack, startNode)
+	for len(stack) > 0 {
+		var last *TreeNode
+		last, stack = stack[len(stack)-1], stack[:len(stack)-1]
+		result = append(result, last.Data)
+		stack = append(stack, last.Children...)
+	}
+	return result
+}
+
+func BFS(startNode *TreeNode) []interface{} {
+	var queue []*TreeNode
+	var result []interface{}
+	queue = append(queue, startNode)
+	for len(queue) > 0 {
+		var first *TreeNode
+		first, queue = queue[0], queue[1:]
+		result = append(result, first.Data)
+		queue = append(queue, first.Children...)
+	}
+	return result
 }
--- main.go
+++ main.go
@@ -2,22 +2,42 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"math/rand"
 
-	"github.com/bbabi0901/dataStructureAlgorithm/algorithm"
-	"github.com/bbabi0901/dataStructureAlgorithm/toy"
+	"github.com/bbabi0901/dataStructureAlgorithm/dataStructure"
 )
 
 func main() {
-	arr := []int{26, 3, 52, 62, 33, 12, 7}
+	/*
+		arr := []int{26, 3, 52, 62, 33, 12, 7}
+		fmt.Println(algorithm.BubbleSort(arr))
+		fmt.Println(algorithm.SelectionSort(arr))
+		fmt.Println(algorithm.InsertionSort(arr))
+		fmt.Println(algorithm.QuickSort(arr))
+		fmt.Println(algorithm.MergeSort(arr))
+	*/
 
-	fmt.Println(algorithm.BubbleSort(arr))
-	fmt.Println(algorithm.SelectionSort(arr))
-	fmt.Println(algorithm.InsertionSort(arr))
-	fmt.Println(algorithm.QuickSort(arr))
-	fmt.Println(algorithm.MergeSort(arr))
+	/*
+		start := time.Now()
+		fmt.Println(toy.Fibo(40), time.Since(start))
+		fmt.Println(toy.Tiling(4))
+	*/
 
-	start := time.Now()
-	fmt.Println(toy.Fibo(40), time.Since(start))
-	fmt.Println(toy.Tiling(4))
+	treeRoot := dataStructure.MakeTreeNode(5)
+	// 자식 4개 추가
+	for i := 0; i < 4; i++ {
+		// rand.Intn returns a random int n, 0 <= n < 100.
+		treeRoot.AddNode(rand.Intn(100))
+	}
+	for i := 0; i < len(treeRoot.Children); i++ {
+		//	손자 3개씩
+		for j := 0; j < 3; j++ {
+			treeRoot.Children[i].AddNode(rand.Intn(100))
+		}
+	}
+
+	fmt.Println(dataStructure.DFSWithRecursive(treeRoot)...)
+	fmt.Println(dataStructure.DFSWithStack(treeRoot)...)
+	// Recursive랑 Stack이랑 탐색 방향이 다르다. -> vs <-
+	fmt.Println(dataStructure.BFS(treeRoot)...)
 }
